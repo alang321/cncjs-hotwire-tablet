9ca75538719adf8a37e6848ddf5c60eed0b1c535
Anton Lang <34992832+alang321@users.noreply.github.com>
2024-02-11 13:12:57 +0100

    a

--- src/app.js
+++ src/app.js
@@ -159,23 +159,20 @@ $(function () {
 
     cnc.goAxis = function (axis, coordinate) {
         cnc.click();
-        controller.command('gcode', 'G90');
-        controller.command('gcode', 'G0 ' + axis + coordinate);
         controller.command('gcode', 'G91');
+        controller.command('gcode', 'G0 ' + axis + coordinate);
     }
 
     cnc.goTwoAxes = function (axis1, axis2, coordinate) {
         cnc.click();
-        controller.command('gcode', 'G90');
-        controller.command('gcode', 'G0 ' + axis1 + coordinate + ' ' + axis2 + coordinate);
         controller.command('gcode', 'G91');
+        controller.command('gcode', 'G0 ' + axis1 + coordinate + ' ' + axis2 + coordinate);
     }
 
     cnc.goAllAxes = function (coordinate) {
         cnc.click();
-        controller.command('gcode', 'G90');
-        controller.command('gcode', 'G0 X' + coordinate + ' Y' + coordinate + ' Z' + coordinate + ' A' + coordinate);
         controller.command('gcode', 'G91');
+        controller.command('gcode', 'G0 X' + coordinate + ' Y' + coordinate + ' Z' + coordinate + ' A' + coordinate);
     }
 
 
